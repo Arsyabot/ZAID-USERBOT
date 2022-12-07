47a6f87e113b8aa071fd93f6895f239ad7fad2a7
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-12-07 07:40:47 +0700

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -5,3 +5,29 @@ from pyrogram import filters
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
    await message.reply_text("Hey Zaid Userbot Assistant here")
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(_, message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/7b2a3fa167686dfaa3da8.jpg",
+        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
+💥 Hᴇʟʟᴏ, I Aᴍ ᴀʟʙʏ ᴘʏʀᴏʙᴏᴛ » Aɴ Aᴅᴠᴀɴᴄᴇᴅ
+Pʀᴇᴍɪᴜᴍ Tᴇʟᴇɢʀᴀᴍ Usᴇʀ Bᴏᴛ.
+┏━━━━━━━━━━━━━━━━━━━┓
+┣★ Oᴡɴᴇʀ'xD› : [ᴀʟʙʏ](https://t.me/Punya_Alby)
+┣★ Uᴘᴅᴀᴛᴇs ›› : [Uᴘᴅᴀᴛᴇs](https://t.me/ruangprojects)
+┣★ Sᴜᴘᴘᴏʀᴛ » : [Dɪsᴄᴜs](https://t.me/ruangdiskusikami)
+┗━━━━━━━━━━━━━━━━━━━┛
+💞 Cʟɪᴄᴋ Oɴ Dᴇᴘʟᴏʏ Bᴜᴛᴛᴏɴ Tᴏ Mᴀᴋᴇ
+Yᴏᴜʀ Oᴡɴ » Gᴇɴɪᴜs Usᴇʀ Bᴏᴛ.
+━━━━━━━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Dᴇᴘʟᴏʏ Aʟʙʏ Pʏʀᴏʙᴏᴛ ✨", url=f"https://github.com/PunyaAlby/ALBY-PYROBOT")
+                ]
+                
+           ]
+        ),
+    )
